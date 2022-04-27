6136882ca50dea34683fae2e4845b4d0f33e09a0
Samuele17 <103572392+Samuele17@users.noreply.github.com>
2022-04-27 12:01:47 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 /**
  * 
  * @author 4c
- *version 
+ *version 2b online
  */
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("prova 2");
+		System.out.println("prova 2b online");
 
 	}
 
