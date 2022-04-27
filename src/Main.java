5919aab3a146025dabc9091381d437b033189d94
4cSamuele <4c@INF14>
2022-04-27 11:54:28 +0200

    versione 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-
+/**
+ * 
+ * @author 4c
+ *version 
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("prova 2");
 
 	}
 
